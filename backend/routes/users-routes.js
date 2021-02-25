20c6ce500d14625da53e37b91fd4f33e909cba5e
Maximillian C <emillian88@gmail.com>
2021-02-25 06:40:08 -0800

    gettin errthang up to github son

---
+++ backend/routes/users-routes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { check } = require("express-validator");
+
+const usersControllers = require("../controllers/users-controllers");
+const fileUpload = require("../services/file-upload");
+
+const router = express.Router();
+
+router.get("/", usersControllers.getUsers);
+
+router.post(
+  "/signup",
+  fileUpload.single('image'),
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersControllers.createUser
+);
+
+router.post(
+  "/login",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersControllers.login
+);
+
+module.exports = router;
