508a2de370cfd8c24f59000887abcdb5076ff497
Maximillian C <emillian88@gmail.com>
2021-05-03 16:54:11 -0700

    Cleanup the squeakqual

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -35,7 +35,7 @@ setDrawerIsOpen(true);
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">YourPlaces</Link>
+          <Link to="/">YourPlants</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
